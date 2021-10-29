ce2d6b41989c27121542d5261e9bafe64be6d5d7
XablauGtx <gustavo2020gts@hotmail.com>
2021-10-29 13:32:45 -0300

    Up

--- header.js
+++ header.js
@@ -1,15 +1,1 @@
-let time = 2000,
-  currentImageIndex = 0,
-  images = document.querySelectorAll('#slides img')
-max = images.length
 
-function nextImage() {
-  currentImageIndex++
-
-  images[currentImageIndex].classList.add('slide-image')
-}
-
-function start() {
-  setInterval(() => {}, time)
-}
-window.addEventListener('load', start)
