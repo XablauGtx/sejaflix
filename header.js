d92f96c02f9be3be407123ce43a17decc48f20d5
XablauGtx <gustavo2020gts@hotmail.com>
2021-11-04 17:21:32 -0300

    Carrosel de Filmes

--- header.js
+++ header.js
@@ -1,1 +1,15 @@
+const arrowsR1 = document.querySelectorAll('.arrowR1')
+const arrowsL1 = document.querySelectorAll('.arrowL1')
+const movielist = document.querySelectorAll('.line1')
 
+arrowsR1.forEach((arrowR1,i) => {
+ arrowR1.addEventListener('click', () => {
+    movielist[i].style.transform = 'translateX(-1345px)'
+  })
+})
+
+arrowsL1.forEach((arrowL1,i) => {
+  arrowL1.addEventListener('click', () => {
+     movielist[i].style.transform = 'translateX(0px)'
+   })
+ })
